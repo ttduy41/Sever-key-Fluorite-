2f5ca2aa9289fcb6868c7dd519ebc62bf9f1e409
ttduy41 <ttduy41@gmail.com>
2026-05-03 12:28:09 +0700

    Add KEY_DATABASE with Fluorite entries

---
+++ database.js
@@ -0,0 +1,19 @@
+const KEY_DATABASE = {
+    "day1": [
+        "Fluorite-ADR-M1N5B8V", 
+        "Fluorite-ADR-A1B2C3D", 
+        "Fluorite-ADR-E5F6G7H", 
+        "Fluorite-ADR-X9Y8Z7W", 
+        "Fluorite-ADR-Q1W2E3R"
+    ],
+    "day7": [
+        "Fluorite-ADR-VIP7D01", 
+        "Fluorite-ADR-VIP7D02", 
+        "Fluorite-ADR-VIP7D03"
+    ],
+    "day30": [
+        "Fluorite-ADR-PRO30D01", 
+        "Fluorite-ADR-PRO30D02", 
+        "Fluorite-ADR-PRO30D03"
+    ]
+};
